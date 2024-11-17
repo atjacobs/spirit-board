fdd4d4f94f0d89208b4470e665b9c5b0da9cb158
Alex Jacobs <alexjacobs@gmail.com>
2024-11-17 07:58:33 -0700

    changed GPIOs, enabled ble?

--- ouija-arduino/src/main.cpp
+++ ouija-arduino/src/main.cpp
@@ -3,21 +3,27 @@
 #include "driver/pcnt.h"
 #include "tlc59401.h"
 #include "ouija.h"
+#include <Wire.h>
+#include <ble.h>
 
 bool toggle = true;
 // char my_message[] = "abcdefghijklmnopqr";
 char my_message[] = "greenfirst";
 
 void setup() {
-
+  Wire.begin(23,22,0);
   exp32_hal_led_init();
   Serial.begin(115200);
   ouija_init();
   ouija_set_string(my_message);
-
+  analogWrite(15, 16);
+  Wire.beginTransmission(0x3B);
+  Wire.write(0xeb);
+  Wire.endTransmission();
+  ble_setup();
 }
 
 void loop() {
-
-  ouija_loop();
+  // analogWrite(current_pin, 16);
+  // ouija_loop();
 }
